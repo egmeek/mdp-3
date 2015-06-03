831c94dcdad2110e1617fc3136eaa4f7c3ab2608
Peter Horvath <horvthpeter@gmail.com>
2015-06-03 15:37:23 +0300

    Implemented eval functions and added tests

---
+++ eval.py
@@ -0,0 +1,209 @@
+from deuces.deuces import Card as DCard, Evaluator as DEvaluator
+from game import Card, suits, ranks
+
+
+class DeucesWrapper(object):
+    def __init__(self):
+        self.e = DEvaluator()
+
+    def evaluate(self, board, hand):
+        dhand = []
+        dboard = []
+        for c in board:
+            dcard = DCard.new(repr(c))
+            dboard.append(dcard)
+        for c in hand.cards():
+            dcard = DCard.new(repr(c))
+            dhand.append(dcard)
+        return self.e.evaluate(dboard, dhand)
+
+
+hand_ranks = {
+'K4s': 57, 'K4o': 131, '64o': 135, 'JJ': 4, 'KQs': 6, '82o': 167, 'Q4s':
+70, '22': 50, 'J3o': 152, '53o': 137, 'K7s': 43, 'A6s': 33, 'A6o': 112, '53s':
+76, '72s': 119, 'Q4o': 142, '92s': 110, 'KQo': 19, '82s': 117, 'QJo': 34,
+'76s': 55, 'K5s': 54, 'T7o': 123, 'K5o': 127, 'T7s': 56, '76o': 120, 'T5o':
+156, 'QJs': 12, 'Q5s': 68, '93o': 164, '55': 45, 'A7s': 29, '54o': 126, 'A7o':
+101, '54s': 64, '65o': 122, '93s': 106, 'Q5o': 140, 'J9s': 25, 'T2o': 161,
+'A4o': 103, 'T4o': 157, 'J5o': 148, '88': 20, 'A4s': 31, 'J9o': 79, 'T2s': 97,
+'43o': 141, '84s': 93, 'J5s': 81, '73s': 102, 'K7o': 121, 'Q2o': 145, '64s':
+69, 'Q2s': 74, '84o': 155, '43s': 83, 'K6s': 52, 'T8o': 99, 'J8s': 40, '98s':
+39, '72o': 168, 'A5o': 100, 'A5s': 27, '98o': 98, 'J8o': 107, '83s': 115,
+'T9o': 72, '42o': 153, '33': 51, '52o': 150, 'Q3o': 143, 'JTs': 15, 'Q3s': 71,
+'T4s': 94, 'JTo': 46, 'J3s': 86, '92o': 165, '42s': 96, '83o': 166, 'T9s': 22,
+'75s': 66, '63s': 89, 'QTs': 14, 'J7s': 63, 'Q8o': 114, 'Q8s': 42, '66': 35,
+'J7o': 128, 'QTo': 48, '75o': 129, '63o': 147, 'A2s': 38, 'K9o': 80, '96s': 67,
+'AKs': 3, 'ATo': 41, '86s': 61, 'AKo': 10, 'ATs': 11, '86o': 125, 'J2o': 154,
+'K9s': 21, 'J2s': 88, '96o': 133, 'A2o': 116, '62s': 109, 'Q7o': 130, 'J6s':
+78, '99': 16, 'J6o': 146, '62o': 162, 'T3s': 95, 'A3s': 32, '97s': 53, '85s':
+77, 'KK': 1, 'T8s': 37, 'KJo': 30, 'Q9s': 24, 'KJs': 8, 'Q9o': 82, '65s': 62,
+'97o': 118, '85o': 138, 'A3o': 108, 'K6o': 124, 'A8s': 23, 'T5s': 92, 'AQs': 5,
+'T3o': 159, 'K2s': 58, '32o': 158, 'K2o': 134, 'AQo': 17, 'A8o': 90, 'Q6o':
+136, '87o': 113, 'TT': 9, '94s': 105, 'T6s': 73, '94o': 163, 'T6o': 139, 'Q6s':
+65, 'AA': 0, 'A9o': 75, '74s': 84, 'K3s': 59, '73o': 160, '52s': 91, 'J4s': 85,
+'77': 28, 'J4o': 151, 'K3o': 132, 'A9s': 18, '74o': 144, 'QQ': 2, 'K8o': 111,
+'95s': 87, 'AJs': 7, '87s': 47, 'KTs': 13, '32s': 104, 'KTo': 44, '95o': 149,
+'AJo': 26, 'K8s': 36, 'Q7s': 60, '44': 49
+}
+
+
+cards = (
+Card(rank='A', suit='h'), Card(rank='A', suit='s'), Card(rank='A', suit='c'),
+Card(rank='A', suit='d'), Card(rank='K', suit='h'), Card(rank='K', suit='s'),
+Card(rank='K', suit='c'), Card(rank='K', suit='d'), Card(rank='Q', suit='h'),
+Card(rank='Q', suit='s'), Card(rank='Q', suit='c'), Card(rank='Q', suit='d'),
+Card(rank='J', suit='h'), Card(rank='J', suit='s'), Card(rank='J', suit='c'),
+Card(rank='J', suit='d'), Card(rank='T', suit='h'), Card(rank='T', suit='s'),
+Card(rank='T', suit='c'), Card(rank='T', suit='d'), Card(rank='9', suit='h'),
+Card(rank='9', suit='s'), Card(rank='9', suit='c'), Card(rank='9', suit='d'),
+Card(rank='8', suit='h'), Card(rank='8', suit='s'), Card(rank='8', suit='c'),
+Card(rank='8', suit='d'), Card(rank='7', suit='h'), Card(rank='7', suit='s'),
+Card(rank='7', suit='c'), Card(rank='7', suit='d'), Card(rank='6', suit='h'),
+Card(rank='6', suit='s'), Card(rank='6', suit='c'), Card(rank='6', suit='d'),
+Card(rank='5', suit='h'), Card(rank='5', suit='s'), Card(rank='5', suit='c'),
+Card(rank='5', suit='d'), Card(rank='4', suit='h'), Card(rank='4', suit='s'),
+Card(rank='4', suit='c'), Card(rank='4', suit='d'), Card(rank='3', suit='h'),
+Card(rank='3', suit='s'), Card(rank='3', suit='c'), Card(rank='3', suit='d'),
+Card(rank='2', suit='h'), Card(rank='2', suit='s'), Card(rank='2', suit='c'),
+Card(rank='2', suit='d'),
+)
+
+
+class Eval(object):
+    def __init__(self):
+        self.relevant = []
+
+    def get_hand_rank(self, cards):
+        c1, c2 = cards
+        if c2.rank_num() > c1.rank_num():
+            c2, c1 = c1, c2
+        suit = ''
+        if c1.rank_num() != c2.rank_num():
+            suit = 'o' if c1.suit != c2.suit else 's'
+        key = c1.rank + c2.rank + suit
+        return hand_ranks[key]
+
+    def check_straight(self, cards):
+        c = 1
+        self.relevant = []
+        for i in xrange(len(cards) - 1):
+            if cards[i].rank_num() - 1 == cards[i+1].rank_num():
+                c += 1
+                if c == 5:
+                    self.relevant.append(cards[i-3])
+                    return True
+            else:
+                c = 1
+        return False
+
+    def check_flush(self, cards):
+        colors = {'s': 0, 'h': 0, 'c': 0, 'd': 0}
+        self.relevant = []
+        for c in cards:
+            colors[c.suit] += 1
+            if colors[c.suit] == 5:
+                for c2 in cards:
+                    if c2.suit == c.suit:
+                        self.relevant.append(c2)
+                self.relevant = self.relevant[:5]
+                return True
+        return False
+
+    def check_kind_4(self, cards):
+        self.relevant = []
+        freq = {}
+        for c in cards:
+            freq[c.rank_num()] = freq.setdefault(c.rank_num(), 0) + 1
+        if freq.values().count(4) != 1:
+            return False
+        high = None
+        for c in cards:
+            if freq[c.rank_num()] == 4:
+                self.relevant.append(c)
+            elif not high:
+                high = c
+        self.relevant.append(high)
+        return True
+
+    def check_full(self, cards):
+        self.relevant = []
+        freq = {}
+        for c in cards:
+            freq[c.rank_num()] = freq.setdefault(c.rank_num(), 0) + 1
+
+        if not (
+            (freq.values().count(3) == 1 and freq.values().count(2) >= 1) or
+            freq.values().count(3) == 2
+        ):
+            return False
+        card2, card3 = None, None
+        for c in cards:
+            if freq[c.rank_num()] >= 3:
+                card3 = c
+                break
+        for c in cards:
+            if freq[c.rank_num()] >= 2 and c.rank_num() != card3.rank_num():
+                card2 = c
+                break
+        for c in cards:
+            if c.rank_num() == card3.rank_num():
+                self.relevant.append(c)
+        k = 0
+        for c in cards:
+            if c.rank_num() == card2.rank_num() and k < 2:
+                self.relevant.append(c)
+                k += 1
+        return True
+
+    def check_kind_3(self, cards):
+        self.relevant = []
+        freq = {}
+        for c in cards:
+            freq[c.rank_num()] = freq.setdefault(c.rank_num(), 0) + 1
+        if 3 not in freq.values():
+            return False
+        for c in cards:
+            if freq[c.rank_num()] == 3:
+                self.relevant.append(c)
+        k = 0
+        for c in cards:
+            if freq[c.rank_num()] < 3 and k < 2:
+                self.relevant.append(c)
+                k += 1
+        return True
+
+    def check_kind_2_2(self, cards):
+        self.relevant = []
+        freq = {}
+        for c in cards:
+            freq[c.rank_num()] = freq.setdefault(c.rank_num(), 0) + 1
+        if freq.values().count(2) < 2:
+            return False
+        k = 0
+        for c in cards:
+            if k < 4:
+                if freq[c.rank_num()] == 2:
+                    self.relevant.append(c)
+                    k += 1
+        for c in cards:
+            if c not in self.relevant:
+                self.relevant.append(c)
+                break
+        return True
+
+    def check_kind_2(self, cards):
+        self.relevant = []
+        freq = {}
+        for c in cards:
+            freq[c.rank_num()] = freq.setdefault(c.rank_num(), 0) + 1
+        if freq.values().count(2) != 1:
+            return False
+        for c in cards:
+            if freq[c.rank_num()] == 2:
+                self.relevant.append(c)
+        k = 0
+        for c in cards:
+            if c not in self.relevant and k < 3:
+                self.relevant.append(c)
+                k += 1
+        return True
---
+++ tests/test_eval.py
@@ -0,0 +1,664 @@
+from unittest import TestCase
+from game import Card, ranks, suits
+from eval import Eval
+
+
+class TestEval(TestCase):
+    def test_check_straight_ok2(self):
+        e = Eval()
+        board = [
+            Card('A', 's'),
+            Card('K', 'c'),
+            Card('Q', 'c'),
+            Card('J', 'c'),
+            Card('T', 'c'),
+        ]
+        self.assertEquals(e.check_straight(board), True)
+        self.assertEquals(e.relevant[0], Card('A', 's'))
+
+    def test_check_straight_bad(self):
+        e = Eval()
+        board = [
+            Card('A', 's'),
+            Card('K', 'c'),
+            Card('J', 'c'),
+            Card('J', 'h'),
+            Card('T', 'c'),
+        ]
+        self.assertEquals(e.check_straight(board), False)
+        self.assertEquals(e.relevant, [])
+
+    def test_check_straight_bad2(self):
+        e = Eval()
+        board = [
+            Card('A', 's'),
+            Card('K', 'c'),
+            Card('Q', 'c'),
+            Card('J', 'h'),
+            Card('9', 'c'),
+        ]
+        self.assertEquals(e.check_straight(board), False)
+        self.assertEquals(e.relevant, [])
+
+    def test_check_straight_bad3(self):
+        e = Eval()
+        board = [
+            Card('K', 's'),
+            Card('K', 'c'),
+            Card('Q', 'c'),
+            Card('J', 'h'),
+            Card('T', 'c'),
+        ]
+        self.assertEquals(e.check_straight(board), False)
+        self.assertEquals(e.relevant, [])
+
+    def test_check_straight_many(self):
+        e = Eval()
+        for i in xrange(len(ranks) - 4):
+            board = []
+            for j in xrange(5):
+                board.append(Card(ranks[i+j], 'h'))
+            self.assertEqual(e.check_straight(board), True)
+            self.assertEqual(e.relevant[0], Card(ranks[i], 'h'))
+            
+    def test_check_straight_many_bad(self):
+        e = Eval()
+        for i in xrange(len(ranks) - 4):
+            board = []
+            for j in xrange(5):
+                if j is 4:
+                    board.append(Card(ranks[i+j-1], 'h'))
+                else:
+                    board.append(Card(ranks[i+j], 'h'))
+            self.assertEqual(e.check_straight(board), False)
+            self.assertEqual(e.relevant, [])
+
+    def test_check_straight_many_7(self):
+        e = Eval()
+        for i in xrange(len(ranks) - 4):
+            board = []
+            for j in xrange(5):
+                board.append(Card(ranks[i+j], 'h'))
+            board += [Card('A', 's'), Card('7', 'c')]
+            self.assertEqual(e.check_straight(board), True)
+            self.assertEqual(e.relevant[0], Card(ranks[i], 'h'))
+            
+    def test_check_straight_many_bad_7(self):
+        e = Eval()
+        for i in xrange(len(ranks) - 4):
+            board = []
+            for j in xrange(5):
+                if j is 4:
+                    board.append(Card(ranks[i+j], 'h'))
+                else:
+                    board.append(Card(ranks[i+j], 'h'))
+            board += [Card('T', 's')]
+            board = [Card('2', 'd')] + board
+            board[3] = board[2]
+            self.assertEqual(e.check_straight(board), False)
+            self.assertEqual(e.relevant, [])
+
+    def test_flush_bad(self):
+        e = Eval()
+        board = [
+            Card('K', 's'),
+            Card('K', 'c'),
+            Card('Q', 'c'),
+            Card('J', 'h'),
+            Card('T', 'c'),
+        ]
+        self.assertEquals(e.check_flush(board), False)
+        self.assertEquals(e.relevant, [])
+
+    def test_flush_ok(self):
+        e = Eval()
+        board = [
+            Card('6', 's'),
+            Card('K', 's'),
+            Card('Q', 's'),
+            Card('J', 's'),
+            Card('T', 's'),
+        ]
+        self.assertEquals(e.check_flush(board), True)
+        self.assertEquals(e.relevant, board)
+
+    def test_flush_ok(self):
+        e = Eval()
+        board = [
+            Card('6', 's'),
+            Card('K', 's'),
+            Card('Q', 's'),
+            Card('J', 's'),
+            Card('T', 's'),
+            Card('7', 's'),
+        ]
+        self.assertEquals(e.check_flush(board), True)
+        self.assertEquals(e.relevant, board[:-1])
+
+    def test_flush_ok2(self):
+        e = Eval()
+        board = [
+            Card('6', 's'),
+            Card('K', 's'),
+            Card('Q', 's'),
+            Card('J', 'h'),
+            Card('T', 's'),
+            Card('7', 's'),
+        ]
+        self.assertEquals(e.check_flush(board), True)
+        board.remove(Card('J', 'h'))
+        self.assertEquals(e.relevant, board)
+
+    def test_flush_ok3(self):
+        e = Eval()
+        board = [
+            Card('6', 's'),
+            Card('K', 'd'),
+            Card('Q', 's'),
+            Card('J', 'h'),
+            Card('T', 's'),
+            Card('7', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_flush(board), True)
+        board.remove(Card('J', 'h'))
+        board.remove(Card('K', 'd'))
+        self.assertEquals(e.relevant, board)
+
+    def test_flush_bad2(self):
+        e = Eval()
+        board = [
+            Card('6', 'd'),
+            Card('K', 'd'),
+            Card('Q', 's'),
+            Card('J', 'h'),
+            Card('T', 's'),
+            Card('7', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_flush(board), False)
+        board.remove(Card('J', 'h'))
+        board.remove(Card('K', 'd'))
+        self.assertEquals(e.relevant, [])
+
+    def test_flush_ok4(self):
+        e = Eval()
+        board = [
+            Card('6', 's'),
+            Card('K', 's'),
+            Card('Q', 's'),
+            Card('J', 's'),
+            Card('T', 's'),
+            Card('7', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_flush(board), True)
+        self.assertEquals(e.relevant, board[:5])
+
+
+    def test_4kind_ok(self):
+        e = Eval()
+        board = [
+            Card('6', 'c'),
+            Card('6', 'h'),
+            Card('6', 'd'),
+            Card('6', 's'),
+            Card('T', 's'),
+            Card('7', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_pairs(board), True)
+        self.assertEquals(e.relevant, board[:4])
+
+    def test_4kind_ok(self):
+        e = Eval()
+        board = [
+            Card('6', 'c'),
+            Card('6', 'h'),
+            Card('6', 'd'),
+            Card('6', 's'),
+            Card('T', 's'),
+            Card('7', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_4(board), True)
+        self.assertEquals(e.relevant, board[:4] + [Card('T', 's')])
+
+    def test_4kind_ok2(self):
+        e = Eval()
+        board = [
+            Card('Q', 's'),
+            Card('6', 'c'),
+            Card('6', 'h'),
+            Card('T', 's'),
+            Card('6', 's'),
+            Card('6', 'd'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_4(board), True)
+        board.remove(Card('T', 's'))
+        board.remove(Card('2', 's'))
+        self.assertEquals(e.relevant, board[1:] + [board[0]])
+
+    def test_4kind_bad(self):
+        e = Eval()
+        board = [
+            Card('6', 'c'),
+            Card('6', 'h'),
+            Card('6', 's'),
+            Card('T', 's'),
+            Card('7', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_4(board), False)
+        self.assertEquals(e.relevant, [])
+
+    def test_full_bad1(self):
+        e = Eval()
+        board = [
+            Card('6', 'c'),
+            Card('6', 'h'),
+            Card('6', 's'),
+            Card('T', 's'),
+            Card('7', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_full(board), False)
+        self.assertEquals(e.relevant, [])
+
+    def test_full_bad2(self):
+        e = Eval()
+        board = [
+            Card('6', 'c'),
+            Card('2', 'h'),
+            Card('6', 's'),
+            Card('T', 's'),
+            Card('7', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_full(board), False)
+        self.assertEquals(e.relevant, [])
+
+    def test_full_ok1(self):
+        e = Eval()
+        board = [
+            Card('6', 'c'),
+            Card('6', 'h'),
+            Card('6', 's'),
+            Card('T', 's'),
+            Card('7', 's'),
+            Card('2', 'd'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_full(board), True)
+        board.remove(Card('T', 's'))
+        board.remove(Card('7', 's'))
+        self.assertEquals(e.relevant, board)
+
+    def test_full_ok2(self):
+        e = Eval()
+        board = [
+            Card('6', 'c'),
+            Card('6', 'h'),
+            Card('6', 's'),
+            Card('T', 's'),
+            Card('2', 'c'),
+            Card('2', 'd'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_full(board), True)
+        board.remove(Card('T', 's'))
+        board.remove(Card('2', 's'))
+        self.assertEquals(e.relevant, board)
+
+    def test_full_ok3(self):
+        e = Eval()
+        board = [
+            Card('A', 'c'),
+            Card('9', 'h'),
+            Card('9', 's'),
+            Card('9', 's'),
+            Card('6', 'c'),
+            Card('6', 'd'),
+            Card('6', 's'),
+        ]
+        self.assertEquals(e.check_full(board), True)
+        board = [
+            Card('9', 'h'),
+            Card('9', 's'),
+            Card('9', 's'),
+            Card('6', 'c'),
+            Card('6', 'd'),
+        ]
+        self.assertEquals(e.relevant, board)
+
+    def test_full_ok3(self):
+        e = Eval()
+        board = [
+            Card('A', 'c'),
+            Card('4', 'h'),
+            Card('4', 's'),
+            Card('3', 's'),
+            Card('3', 'c'),
+            Card('3', 'd'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_full(board), True)
+        board = [
+            Card('3', 's'),
+            Card('3', 'c'),
+            Card('3', 'd'),
+            Card('4', 'h'),
+            Card('4', 's'),
+        ]
+        self.assertEquals(e.relevant, board)
+
+    def test_full_ok_many1(self):
+        e = Eval()
+        board = []
+        for i in xrange(len(ranks) - 2):
+            for j in xrange(i + 1, len(ranks)):
+                board = []
+                board2 = []
+                for s in suits[:-1]:
+                    board.append(Card(ranks[i], s))
+                for s in suits[:-2]:
+                    board.append(Card(ranks[j], s))
+                self.assertEquals(e.check_full(board), True)
+                self.assertEquals(e.relevant, board)
+
+    def test_full_ok_many2(self):
+        e = Eval()
+        board = []
+        for i in xrange(len(ranks) - 2):
+            for j in xrange(i + 1, len(ranks)):
+                board = []
+                board2 = []
+                for s in suits[:-2]:
+                    board.append(Card(ranks[i], s))
+                for s in suits[:-1]:
+                    board.append(Card(ranks[j], s))
+                self.assertEquals(e.check_full(board), True)
+                self.assertEquals(e.relevant, board[2:] + board[:2])
+
+    def test_kind_3_ok(self):
+        e = Eval()
+        board = [
+            Card('A', 'c'),
+            Card('9', 'h'),
+            Card('8', 's'),
+            Card('3', 's'),
+            Card('3', 'c'),
+            Card('3', 'd'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_3(board), True)
+        board = [
+            Card('3', 's'),
+            Card('3', 'c'),
+            Card('3', 'd'),
+            Card('A', 'c'),
+            Card('9', 'h'),
+        ]
+        self.assertEquals(e.relevant, board)
+
+    def test_kind_3_ok2(self):
+        e = Eval()
+        board = [
+            Card('A', 'c'),
+            Card('A', 'h'),
+            Card('A', 's'),
+            Card('6', 's'),
+            Card('5', 'c'),
+            Card('3', 'd'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_3(board), True)
+        board = [
+            Card('A', 'c'),
+            Card('A', 'h'),
+            Card('A', 's'),
+            Card('6', 's'),
+            Card('5', 'c'),
+        ]
+        self.assertEquals(e.relevant, board)
+
+    def test_kind_3_ok3(self):
+        e = Eval()
+        board = [
+            Card('A', 'c'),
+            Card('Q', 'h'),
+            Card('T', 's'),
+            Card('6', 's'),
+            Card('5', 'c'),
+            Card('5', 'd'),
+            Card('5', 's'),
+        ]
+        self.assertEquals(e.check_kind_3(board), True)
+        board = [
+            Card('5', 'c'),
+            Card('5', 'd'),
+            Card('5', 's'),
+            Card('A', 'c'),
+            Card('Q', 'h'),
+        ]
+        self.assertEquals(e.relevant, board)
+
+    def test_kind_3_ok4(self):
+        e = Eval()
+        board = [
+            Card('T', 's'),
+            Card('6', 's'),
+            Card('5', 'c'),
+            Card('5', 'd'),
+            Card('5', 's'),
+        ]
+        self.assertEquals(e.check_kind_3(board), True)
+        board = [
+            Card('5', 'c'),
+            Card('5', 'd'),
+            Card('5', 's'),
+            Card('T', 's'),
+            Card('6', 's'),
+        ]
+        self.assertEquals(e.relevant, board)
+
+    def test_kind_3_bad1(self):
+        e = Eval()
+        board = [
+            Card('T', 's'),
+            Card('6', 's'),
+            Card('6', 'c'),
+            Card('5', 'd'),
+            Card('5', 's'),
+        ]
+        self.assertEquals(e.check_kind_3(board), False)
+        self.assertEquals(e.relevant, [])
+
+    def test_kind_3_bad2(self):
+        e = Eval()
+        board = [
+            Card('T', 's'),
+            Card('T', 's'),
+            Card('6', 's'),
+            Card('6', 'c'),
+            Card('5', 'd'),
+            Card('5', 's'),
+            Card('3', 's'),
+        ]
+        self.assertEquals(e.check_kind_3(board), False)
+        self.assertEquals(e.relevant, [])
+
+    def test_kind_2_2_ok(self):
+        e = Eval()
+        board = [
+            Card('T', 's'),
+            Card('6', 's'),
+            Card('6', 'c'),
+            Card('5', 'd'),
+            Card('5', 's'),
+        ]
+        self.assertEquals(e.check_kind_2_2(board), True)
+        board = [
+            Card('6', 's'),
+            Card('6', 'c'),
+            Card('5', 'd'),
+            Card('5', 's'),
+            Card('T', 's'),
+        ]
+        self.assertEquals(e.relevant, board)
+
+    def test_kind_2_2_ok2(self):
+        e = Eval()
+        board = [
+            Card('T', 's'),
+            Card('T', 's'),
+            Card('6', 'c'),
+            Card('5', 'd'),
+            Card('5', 's'),
+        ]
+        self.assertEquals(e.check_kind_2_2(board), True)
+        board = [
+            Card('T', 's'),
+            Card('T', 's'),
+            Card('5', 'd'),
+            Card('5', 's'),
+            Card('6', 'c'),
+        ]
+        self.assertEquals(e.relevant, board)
+
+    def test_kind_2_2_ok3(self):
+        e = Eval()
+        board = [
+            Card('T', 's'),
+            Card('T', 's'),
+            Card('6', 'c'),
+            Card('5', 'd'),
+            Card('5', 's'),
+            Card('2', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_2_2(board), True)
+        board = [
+            Card('T', 's'),
+            Card('T', 's'),
+            Card('5', 'd'),
+            Card('5', 's'),
+            Card('6', 'c'),
+        ]
+        self.assertEquals(e.relevant, board)
+
+    def test_kind_2_2_ok4(self):
+        e = Eval()
+        board = [
+            Card('T', 's'),
+            Card('T', 's'),
+            Card('7', 'c'),
+            Card('6', 'd'),
+            Card('5', 's'),
+            Card('2', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_2_2(board), True)
+        board = [
+            Card('T', 's'),
+            Card('T', 's'),
+            Card('2', 's'),
+            Card('2', 's'),
+            Card('7', 'c'),
+        ]
+        self.assertEquals(e.relevant, board)
+
+    def test_kind_2_2_bad1(self):
+        e = Eval()
+        board = [
+            Card('T', 's'),
+            Card('T', 's'),
+            Card('7', 'c'),
+            Card('6', 'd'),
+            Card('5', 's'),
+            Card('4', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_2_2(board), False)
+        self.assertEquals(e.relevant, [])
+
+    def test_kind_2_2_bad2(self):
+        e = Eval()
+        board = [
+            Card('T', 's'),
+            Card('9', 's'),
+            Card('7', 'c'),
+            Card('6', 'd'),
+            Card('5', 's'),
+            Card('5', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_2_2(board), False)
+        self.assertEquals(e.relevant, [])
+
+    def test_kind_2_2_bad3(self):
+        e = Eval()
+        board = [
+            Card('7', 'c'),
+            Card('6', 'd'),
+            Card('5', 's'),
+            Card('5', 's'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_2_2(board), False)
+        self.assertEquals(e.relevant, [])
+
+    def test_kind_2_ok1(self):
+        e = Eval()
+        board = [
+            Card('T', 's'),
+            Card('T', 's'),
+            Card('6', 'c'),
+            Card('4', 'd'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_2(board), True)
+        board = [
+            Card('T', 's'),
+            Card('T', 's'),
+            Card('6', 'c'),
+            Card('4', 'd'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.relevant, board)
+
+    def test_kind_2_ok2(self):
+        e = Eval()
+        board = [
+            Card('T', 's'),
+            Card('9', 's'),
+            Card('8', 's'),
+            Card('8', 's'),
+            Card('6', 'c'),
+            Card('4', 'd'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_2(board), True)
+        board = [
+            Card('8', 's'),
+            Card('8', 's'),
+            Card('T', 's'),
+            Card('9', 's'),
+            Card('6', 'c'),
+        ]
+        self.assertEquals(e.relevant, board)
+
+    def test_kind_2_bad(self):
+        e = Eval()
+        board = [
+            Card('T', 's'),
+            Card('9', 's'),
+            Card('8', 's'),
+            Card('7', 's'),
+            Card('6', 'c'),
+            Card('4', 'd'),
+            Card('2', 's'),
+        ]
+        self.assertEquals(e.check_kind_2(board), False)
+        self.assertEquals(e.relevant, [])
